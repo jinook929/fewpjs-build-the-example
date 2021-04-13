a3b7d8bf4ea3e6bfe91185fa1b2c0214c4bcfe65
jinook929 <jinook929@gmail.com>
2021-04-13 10:48:22 -0400

    commit tree 27df2d15100732469d9a13550d6bc9a320efa3b8

--- main.js
+++ main.js
@@ -4,8 +4,40 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// const modal = document.querySelector("#modal");
+// modal.classList.add("hidden")
 
+const like_glyph = document.querySelectorAll(".like-glyph");
+like_glyph.forEach((heartIcon) => {
+  heartIcon.addEventListener("click", e => {
+    mimicServerCall()
+    .then(obj => {
+      console.log(obj);
+      flipHeart(e);
+    })
+    .catch(error => {
+      console.log(error);
+      const modal = document.querySelector("#modal");
+      const modal_message = document.querySelector("#modal-message");
+      // console.log(modal.firstElementChild.innerHTML);
+      modal_message.innerHTML = error;
+      modal.classList.remove("hidden");
+      setTimeout(() => { modal.classList.add("hidden"); }, 5000);
+    });
+  });
+});
 
+function flipHeart(event) {
+  if(event.target.textContent == '♡') {
+    event.target.textContent = FULL_HEART;
+    console.log(event.target)
+    event.target.classList.add("activated-heart");
+  } else {
+    event.target.textContent = EMPTY_HEART;
+    event.target.classList.remove("activated-heart");
+    console.log(event.target)
+  }
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
